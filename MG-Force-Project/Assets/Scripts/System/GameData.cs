358e92d69ee77a6b1c5bea3c0e6effbb2c0a1552
riku2202 <tsukayoshi2391@gmail.com>
2024-11-09 17:05:21 +0900

    ゲームデータ管理処理の作成

---
+++ MG-Force-Project/Assets/Scripts/System/GameData.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace System 
+{
+    /// <summary>
+    /// ゲームデータ
+    /// </summary>
+    [System.Serializable]
+    public class GameData
+    {
+        // ステージの最大数
+        private const int STAGE_MAX_NUM = 8;
+
+        // ステージのクリアフラグ
+        public bool[] IsClearStage = new bool[STAGE_MAX_NUM];
+    }
+}
--- MG-Force-Project/Assets/Scripts/System/StageData.cs
+++ MG-Force-Project/Assets/Scripts/System/StageData.cs
@@ -2,14 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "StageData", menuName = "Game/Stage Data")]
-public class StageData : ScriptableObject
+namespace System
 {
-    // ステージの管理番号
-    [SerializeField]
-    private int StageIndex;
+    /// <summary>
+    /// ステージデータ
+    /// </summary>
+    [CreateAssetMenu(fileName = "StageData", menuName = "Game/Stage Data")]
+    public class StageData : ScriptableObject
+    {
+        // ステージの管理番号
+        [SerializeField]
+        private int StageIndex;
 
-    // ステージのプレハブ
-    [SerializeField]
-    private GameObject StagePrefab;
+        // ステージのプレハブ
+        [SerializeField]
+        private GameObject StagePrefab;
+    }
 }
