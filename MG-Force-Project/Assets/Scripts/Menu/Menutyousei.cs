e567160fbe69711b5c60bd0040970690ed4eced2
daidai0729D <ueyama6727@gmail.com>
2024-12-11 18:04:36 +0900

    メニュー画面

--- MG-Force-Project/Assets/Scripts/Menu/Menutyousei.cs
+++ MG-Force-Project/Assets/Scripts/Menu/Menutyousei.cs
@@ -1,51 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Menutyousei : MonoBehaviour
 {
-    [SerializeField] GameObject MenuObject;
+    //[SerializeField] private GameObject MenuPanel;
 
-    bool menuzyoutai;
+    //private bool isMenuOpen = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //if (MenuPanel != null)
+        //{
+        //    MenuPanel.SetActive(false);
+        //}
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (MenuObject  == false)
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (Input.GetKeyDown(KeyCode.Escape) == true)
-            {
-                MenuObject.gameObject.SetActive(true);
-                menuzyoutai = true;
+            Application.Quit();
+
+#if UNITY_EDITOR
+
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
 
 
-                // マウスカーソルを表示にし、位置固定解除
-                Cursor.visible = true;
-                Cursor.lockState = CursorLockMode.None;
 
-
-            }
-        }
-
-        else
-        {
-            if (Input.GetKeyDown(KeyCode.Escape) == false)
-            {
-                MenuObject.gameObject.SetActive(false);
-                menuzyoutai = false;
-
-                // マウスカーソルを非表示にし、位置を固定
-                Cursor.visible = false;
-                Cursor.lockState = CursorLockMode.Locked;
-
-
-            }
         }
     }
+
+    private void ToogleMenu()
+    {
+        //if (MenuPanel != null)
+        //{
+        //    isMenuOpen = !isMenuOpen;
+
+        //    MenuPanel.SetActive(isMenuOpen);
+
+        //    Time.timeScale = isMenuOpen ? 0 : 1;
+
+        //    Cursor.visible = isMenuOpen;
+        //    Cursor.lockState = isMenuOpen ? CursorLockMode.None : CursorLockMode.Locked;
+        //}
+    }
+
+    //private void OpenMenu()
+    //{
+    //    if (MenuPanel != null)
+    //    {
+    //        isMenuOpen = true;
+    //        MenuPanel.SetActive(true);
+    //        Time.timeScale = 0;
+    //        Cursor.visible = true;
+    //        Cursor.lockState = CursorLockMode.None;
+    //    }
+    //}
+
+    //private void CloseMenu()
+    //{
+    //    if (MenuPanel != null)
+    //    {
+    //        isMenuOpen = false;
+    //        MenuPanel.SetActive(false);
+    //        Time.timeScale = 1;
+    //        Cursor.visible = false;
+    //        Cursor.lockState = CursorLockMode.Locked;
+    //    }
+    //}
+
+
+
 }
