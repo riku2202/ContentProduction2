44676d46110b6253cf2a0dc759c495284f6636f8
yu-ki-rohi <yukihiro4924@gmail.com>
2025-01-29 18:46:25 +0900

    Logの非表示化およびコメントの追加

--- MG-Force-Project/Assets/Scripts/StageScene/Player/PlayerMoveController.cs
+++ MG-Force-Project/Assets/Scripts/StageScene/Player/PlayerMoveController.cs
@@ -57,8 +57,13 @@ namespace Game.StageScene.Player
                 }
             }
 
+#if false
             DebugManager.LogMessage($"{_isGrounded}");
             Debug.DrawRay(GameObject.Find("hip").transform.position, raycastDir, Color.red, RAYCAST_LENGTH);
+#endif
+            // @yu-kirohi
+            // 多分JumpUpdate内でやってることと同じだと思う
+            // あとUpdate内でFind使ってるのは重くなる要因かも
             _isGrounded = Physics.Raycast(GameObject.Find("hip").transform.position, raycastDir, RAYCAST_LENGTH);
 
             _rigidbody.velocity = moveDir;
